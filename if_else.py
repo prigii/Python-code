c946618ddd86c9da2d88b0e52b4c23dfc6d49f34
Peter Thuo <122869546+prigii@users.noreply.github.com>
2023-01-20 16:26:23 +0300

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,9 @@
+Thomas_age = 0
+Kindergaten_Age = 5
+
+if Thomas_age < 5:
+    print('Thomas should be in Pre-shool')
+elif Thomas_age == 5:
+    print('Welcome to kingdergaten Thomas')
+else:
+    print('Thomas should be in another class')
---
+++ integers.py
@@ -0,0 +1,7 @@
+def laugh(lst):
+   if 'True' in lst:
+       return 'haha'
+   else:
+        return 'uh'
+lst = ['False', 'True', 'Damn']
+print(laugh)
